8fb9fe66067a70324263070d432dd78f994993c9
gjh0828 <72548655+gjh0828@users.noreply.github.com>
2024-11-21 17:01:46 +0800

    Update 5. Interaction.R

--- 5. Interaction.R
+++ 5. Interaction.R
@@ -15,7 +15,7 @@ result_interaction1 <- rbindlist(lapply(colnames(df)[c(3,5,7,9,11,13,15,17)], fu
   print(x_)
   df$var <- paste0(df[[x_]], df$p21022_1)
   a <- paste0(setdiff(colnames(df)[c(3,5,7,9,11,13,15,17)], x_), collapse = "+")
-  FML <- as.formula(paste0("Surv(time, outcome == 1) ~ Family_history + var + ",a,"+ p31+p21022+p54_i0+p21000_i0+Family_history"))
+  FML <- as.formula(paste0("Surv(time, outcome == 1) ~ var + ",a,"+ p31+p21022+p54_i0+p21000_i0+Family_history"))
   fit <- coxph(FML, data = df)
   a1 <- as.data.frame(summary(fit)[["coefficients"]])[c(2:9),] %>%
     mutate(Var = rownames(.), Var1 = str_sub(Var, 4, 4), Var2 = str_sub(Var, 5, 5), Var3 = "-1", Var4=x_,
